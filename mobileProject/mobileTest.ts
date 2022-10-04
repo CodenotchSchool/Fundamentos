a4de4014ed06cff873957902db37f40f8ce26c03
CodenotchSchool <codenotch@codenotch.com>
2022-10-04 18:07:30 +0200

    Subida de Prueba gitignore

--- mobileProject/mobileTest.ts
+++ mobileProject/mobileTest.ts
@@ -3,7 +3,7 @@ import { Mobile } from "./mobile";
 let myMobile = new Mobile("iPhone 6", "Apple", "iPhone 6", "Rojo", 1200);
 
 console.log(myMobile.getColor(), 
-            myMobile.getModel(), 
+            myMobile.getModel(),  
             myMobile.getName(), 
             myMobile.getPrice(), 
             myMobile.getTradeMark());
