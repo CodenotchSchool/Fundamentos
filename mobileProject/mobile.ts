9a576356c40ff0e677d615fbcc41787a5453a932
CodenotchSchool <codenotch@codenotch.com>
2022-10-04 17:54:13 +0200

    Subida de la clase Mobile

---
+++ mobileProject/mobile.ts
@@ -0,0 +1,62 @@
+export class Mobile
+{
+    constructor(private name: string, 
+                private tradeMark:string,
+                private model: string,
+                private color: string,
+                private price: number)
+    {}
+
+    public setName(name: string)
+    {
+        this.name = name;
+    }
+
+    public setTradeMark(tradeMark: string)
+    {
+        this.tradeMark = tradeMark;
+    }
+
+    public setModel(model: string)
+    {
+        this.model = model;
+    }
+
+    public setColor(color: string)
+    {
+        this.color = color;
+    }
+
+    public setPrice(price: number)
+    {
+        this.price = price;
+    }
+
+
+    public getName():string
+    {
+        return this.name;
+    }
+
+    public getTradeMark():string
+    {
+        return this.tradeMark;
+    }
+
+    public getModel():string
+    {
+        return this.model;
+    }
+
+    public getColor():string
+    {
+        return this.color;
+    }
+
+    public getPrice():number
+    {
+        return this.price;
+    }
+
+
+}
---
+++ mobileProject/mobileTest.ts
@@ -0,0 +1,21 @@
+import { Mobile } from "./mobile";
+
+let myMobile = new Mobile("iPhone 6", "Apple", "iPhone 6", "Rojo", 1200);
+
+console.log(myMobile.getColor(), 
+            myMobile.getModel(), 
+            myMobile.getName(), 
+            myMobile.getPrice(), 
+            myMobile.getTradeMark());
+
+myMobile.setColor("Naranja");
+myMobile.setModel("iphone 16");
+myMobile.setName("Mi Nuevo iPhone");
+myMobile.setPrice(2000);
+myMobile.setTradeMark("new Apple");
+
+console.log(myMobile.getColor(), 
+            myMobile.getModel(), 
+            myMobile.getName(), 
+            myMobile.getPrice(), 
+            myMobile.getTradeMark());
