9adc813bb0c7da0b6301f3e1bb3faaa89898d1e3
CodenotchSchool <codenotch@codenotch.com>
2022-10-05 12:55:36 +0200

    Subida con los métodos de distancia

--- algebraProject/point.ts
+++ algebraProject/point.ts
@@ -28,5 +28,18 @@ export class Point
         return "(" + this.x + ", " + this.y + ")";
     }
 
+    public distanceToOrigin():number
+    {
+        let result = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+        return result;
+    }
+
+    public calculateDistance(point:Point):number
+    {
+        let result = Math.sqrt(Math.pow( point.x - this.x, 2) + 
+                               Math.pow( point.y - this.y, 2) );
+        return result;
+    }
+
 
 }
--- algebraProject/pointTest.ts
+++ algebraProject/pointTest.ts
@@ -7,3 +7,5 @@ myPoint.setX(30);
 myPoint.setY(40);
 console.log(myPoint.getX())
 console.log(myPoint.getY())
+console.log(myPoint.distanceToOrigin())
+console.log(myPoint.calculateDistance(new Point(50,50)));
